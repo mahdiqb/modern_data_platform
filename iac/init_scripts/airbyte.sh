d72b370ac741a2ccec749c0d88a7be0686349b01
mahdiqb <mahdikarabibene@yahoo.com>
2021-12-22 11:38:27 +0100

    Update Airbyte init script

--- iac/init_scripts/airbyte.sh
+++ iac/init_scripts/airbyte.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 # Setup follows the steps provided by Airbyte
 # Documentation link: https://docs.airbyte.io/deploying-airbyte/on-gcp-compute-engine
+
 # First we install Docker
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
@@ -8,11 +11,13 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debi
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -a -G docker $USER
+
 # Then we install Docker Compose
 sudo apt-get -y install wget
 sudo wget https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -O /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
 # Lastly we install and start Airbyte
 mkdir airbyte && cd airbyte
 wget https://raw.githubusercontent.com/airbytehq/airbyte/master/{.env,docker-compose.yaml}
