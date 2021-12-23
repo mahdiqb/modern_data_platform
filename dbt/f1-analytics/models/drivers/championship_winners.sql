caf5a0c5a03953c28e5a8d844a4ee347144c1ef8
mahdiqb <mahdikarabibene@yahoo.com>
2021-12-23 18:14:22 +0100

    Add sample dbt model

--- dbt/f1-analytics/models/drivers/championship_winners.sql
+++ dbt/f1-analytics/models/drivers/championship_winners.sql
@@ -4,8 +4,8 @@ WITH season_results AS(
         year,
         SUM(points) as points_total
     FROM
-        `poc-dbt-333912.f1.results` AS res
-        INNER JOIN `poc-dbt-333912.f1.races` AS rc ON res.raceId = rc.raceId
+        `project.f1.results` AS res
+        INNER JOIN `project.f1.races` AS rc ON res.raceId = rc.raceId
     GROUP BY
         res.driverId,
         year
@@ -23,6 +23,6 @@ SELECT
     CONCAT(d.forename, " ", d.surname) AS full_name
 FROM
     season_ranking s
-    INNER JOIN `poc-dbt-333912.f1.drivers` d ON d.driverId = s.driverId
+    INNER JOIN `project.f1.drivers` d ON d.driverId = s.driverId
 WHERE
     ranking = 1
